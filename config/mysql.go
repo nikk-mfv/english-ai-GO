841611e72cba4dc92e1c6f75195ad9e8cc878bae
Jayce <maihoang171@gmail.com>
2025-06-10 20:50:13 +0700

    update CA_CERT env

--- config/mysql.go
+++ config/mysql.go
@@ -1,10 +1,13 @@
 package config
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"log"
 	"os"
 
+	mysqlDriver "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -14,7 +17,35 @@ var (
 )
 
 func ConnectDatabase() {
-	dsn := os.Getenv("DB_DSN")
+	caCert := os.Getenv("CA_CERT")
+	if caCert == "" {
+		log.Fatal("missing CA_CERT env")
+	}
+	caPath := "aiven-ca.pem"
+	err := os.WriteFile(caPath, []byte(caCert), 0644)
+	if err != nil {
+		log.Fatal("failed to write CA cert file:", err)
+	}
+
+	// Load CA cert
+	rootCertPool := x509.NewCertPool()
+	pem, err := os.ReadFile(caPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
+		log.Fatal("Failed to append PEM.")
+	}
+
+	// Register custom TLS config
+	err = mysqlDriver.RegisterTLSConfig("aiven", &tls.Config{
+		RootCAs: rootCertPool,
+	})
+	if err != nil {
+		log.Fatal("Failed to register TLS config: ", err)
+	}
+
+	dsn := os.Getenv("DB_DSN") + "&tls=aiven"
 	if dsn == "" {
 		log.Fatal("missing database connection string")
 	}
